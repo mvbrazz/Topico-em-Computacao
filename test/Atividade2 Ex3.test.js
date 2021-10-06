2b24c518bc56126fe197e335de17688eaae3782e
Marcus Vinicus <mvbrazz@hotmail.com>
2021-10-06 18:59:55 -0300

    Atualização

--- test/Atividade2 Ex3.test.js
+++ test/Atividade2 Ex3.test.js
@@ -14,4 +14,55 @@ describe('Atv2 Ex3',()=>{
         expect(aux).toBe(1);
     });
 
+    test('Verificar classificação abaixo do peso', () => {
+        let pessoa = {};
+        pessoa.peso = 50;
+        pessoa.altura = 1.75;
+        var IMC = calcularIMC(pessoa);
+                 
+        expect(IMC.classificacao).toBe("abaixo do peso");  
+
+    });
+
+    test('Verificar classificação normal', () => {
+        let pessoa = {};
+        pessoa.peso = 60;
+        pessoa.altura = 1.75;
+        var IMC = calcularIMC(pessoa);
+                 
+        expect(IMC.classificacao).toBe("normal");  
+
+    });
+
+    test('Verificar classificação acima do peso', () => {
+        let pessoa = {};
+        pessoa.peso = 80;
+        pessoa.altura = 1.75;
+        var IMC = calcularIMC(pessoa);
+                 
+        expect(IMC.classificacao).toBe("acima do peso");  
+
+    });
+
+    test('Verificar classificação obeso', () => {
+        let pessoa = {};
+        pessoa.peso = 95;
+        pessoa.altura = 1.75;
+        var IMC = calcularIMC(pessoa);
+                 
+        expect(IMC.classificacao).toBe("obeso");  
+
+    });
+
+    test('Verifica IMC', () => {
+        let pessoa = {};
+        pessoa.peso = 60;
+        pessoa.altura = 1.75;
+        var IMC = calcularIMC(pessoa);
+                 
+        expect(IMC.imc).toBe(19.591836734693878);  
+
+    });
+
+
 })
